9f8c37e08e0dbdb2af137336f5b3d6cd4193eab7
antkovalev <antkovalev@azbukavkusa.ru>
2024-08-23 20:33:59 +0300

    fix

--- home/akov/mysql/deploy.sh
+++ home/akov/mysql/deploy.sh
@@ -1,6 +1,7 @@
 docker network create -d bridge weather_bot_network
 docker run -p 3306:3306 \
  --name=mysql -d \
+ --restart=on-failure \
  --env-file=.env \
  -v ./data:/var/lib/mysql \
  --network=weather_bot_network \
