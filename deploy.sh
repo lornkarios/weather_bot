0ac9dd1bbe8d09855341230d0e0ab540ceb21aa8
antkovalev <antkovalev@azbukavkusa.ru>
2024-08-23 14:48:55 +0300

    fix

--- deploy.sh
+++ deploy.sh
@@ -6,5 +6,5 @@ docker image rm lornkarios/weather-api-bot
 echo "Pull image"
 docker pull lornkarios/weather-api-bot
 echo "Start php container"
-docker run -p 80:80 -p 9000:9000 --name php -d lornkarios/weather-api-bot
+docker run -p 80:8080 -p 9000:9000 --name php -d lornkarios/weather-api-bot
 echo "Finish deploying!"
