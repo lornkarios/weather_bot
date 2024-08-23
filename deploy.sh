17e35edc4e33a203797eec9e193f31f58b663e5b
antkovalev <antkovalev@azbukavkusa.ru>
2024-08-23 13:40:29 +0300

    fix

--- deploy.sh
+++ deploy.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 echo "Stop container"
-docker stop backend
-docker rm backend
+docker stop php
+docker rm php
 docker image rm lornkarios/weather-api-bot
 echo "Pull image"
 docker pull lornkarios/weather-api-bot
-echo "Start frontend container"
-docker run -p 80:80 -p 9000:9000 --name backend -d lornkarios/weather-api-bot
+echo "Start php container"
+docker run -p 80:80 -p 9000:9000 --name php -d lornkarios/weather-api-bot
 echo "Finish deploying!"
