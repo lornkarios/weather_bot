98d43e2a91c60a5c1631144d2a17f98141bd266e
antkovalev <antkovalev@azbukavkusa.ru>
2024-08-25 10:20:25 +0300

    add certbot

--- home/akov/mysql/deploy.sh
+++ home/akov/mysql/deploy.sh
@@ -4,5 +4,6 @@ docker run -p 3306:3306 \
  --restart=on-failure \
  --env-file=.env \
  -v ./data:/var/lib/mysql \
+ -v my.cnf:/etc/mysql/conf.d/my.cnf \
  --network=weather_bot_network \
 mysql:8.0.25
--- home/akov/php/deploy.sh
+++ home/akov/php/deploy.sh
@@ -10,5 +10,6 @@ cd /home/akov/php
 docker run -p 80:8080 -p 9000:9000 \
   --name php -d --env-file=.env \
   --network=weather_bot_network \
+  -v .well-known:/var/www/html/.well-known \
   lornkarios/weather-api-bot
 echo "Finish deploying!"
