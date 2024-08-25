512dd5bbddccab44d7ea8089f8ab4a6b5e4f3414
antkovalev <antkovalev@azbukavkusa.ru>
2024-08-25 13:38:17 +0300

    add accept/header middleware

---
+++ app/app/Http/Middleware/JsonAcceptHeader.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+
+class JsonAcceptHeader
+{
+    /**
+     * Get the path the user should be redirected to when they are not authenticated.
+     *
+     * @param \Illuminate\Http\Request $request
+     * @return string|null
+     */
+    public function handle(Request $request, Closure $next)
+    {
+        $request->headers->set('Accept', 'application/json');
+        return $next($request);
+    }
+}
--- app/bootstrap/app.php
+++ app/bootstrap/app.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Middleware\JsonAcceptHeader;
 use App\Http\Middleware\RequestLogger;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
@@ -13,6 +14,7 @@ return Application::configure(basePath: dirname(__DIR__))
     )
     ->withMiddleware(function (Middleware $middleware) {
         $middleware->append(RequestLogger::class);
+        $middleware->append(JsonAcceptHeader::class);
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
