f095f2a345ffc0d0e39dfbd34e45303690a2320e
antkovalev <antkovalev@azbukavkusa.ru>
2024-08-25 11:14:12 +0300

    add certs

--- home/akov/php/deploy.sh
+++ home/akov/php/deploy.sh
@@ -7,10 +7,10 @@ echo "Pull image"
 docker pull lornkarios/weather-api-bot
 echo "Start php container"
 cd /home/akov/php
-docker run -p 80:8080 -p 9000:9000 \
+docker run -p 443:443 -p 9000:9000 \
   --name php -d --env-file=.env \
   --network=weather_bot_network \
   -v ./.well-known:/var/www/html/.well-known \
-  -v ./certificates:/etc/letsencrypt/live \
+  -v /etc/letsencrypt:/etc/letsencrypt \
   lornkarios/weather-api-bot
 echo "Finish deploying!"
