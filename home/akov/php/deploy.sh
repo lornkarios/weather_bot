359b5538362ec3681aa97dd7f1b83bb004fec86f
antkovalev <antkovalev@azbukavkusa.ru>
2024-10-25 14:49:32 +0300

    fix

--- home/akov/php/deploy.sh
+++ home/akov/php/deploy.sh
@@ -7,10 +7,7 @@ echo "Pull image"
 docker pull lornkarios/weather-api-bot
 echo "Start php container"
 cd /home/akov/php
-docker run -p 443:443 -p 9000:9000 \
-  --name php -d --env-file=.env \
-  --network=weather_bot_network \
+docker run
   -v ./.well-known:/var/www/html/.well-known \
-  -v /etc/letsencrypt:/etc/letsencrypt \
   lornkarios/weather-api-bot
 echo "Finish deploying!"
